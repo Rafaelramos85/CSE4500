44dc9e41fafe89fc588419e1cd3ead5e62ce20e4
Rafaelramos85 <43967692+Rafaelramos85@users.noreply.github.com>
2021-04-15 18:59:51 -0700

    Add files via upload

---
+++ json_test.js
@@ -0,0 +1,16 @@
+const names = [
+    {
+      "id": "1",
+      "type": "Human",
+      "name": "Rafael Ramos",
+      "school": "CSUSB",
+      "bio": "This allows us to access local JSON files."
+    },
+    {
+      "id": "2",
+      "type": "cyborg",
+      "name": "Gerald perkins",
+      "School": "none",
+      "bio": "second array"
+    }
+  ]
