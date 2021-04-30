5b7892dfb2b98d2c728c1fdd57202aa649d76a81
Rafaelramos85 <43967692+Rafaelramos85@users.noreply.github.com>
2021-04-29 20:41:07 -0700

    Add files via upload

--- json_test.js
+++ json_test.js
@@ -1,16 +1,36 @@
-const names = [
-    {
-      "id": "1",
-      "type": "Human",
-      "name": "Rafael Ramos",
-      "school": "CSUSB",
-      "bio": "This allows us to access local JSON files."
-    },
-    {
-      "id": "2",
-      "type": "cyborg",
-      "name": "Gerald perkins",
-      "School": "none",
-      "bio": "second array"
-    }
-  ]
+const student = {
+  name :[ '"Rafael Ramos"' ,  '"Gerald Perkins"'],
+  height :[ '"6ft"', '"5ft 10in"' ],
+  age : [ 24, 45 ],
+  school : ['"CSUSB"' , '"none"' ],
+  major : ['"CSE"', '"none"' ]
+
+}
+console.log(student);
+
+// turns student into a string
+const studentToString = JSON.stringify(student)
+
+// sturns student into an object
+const studentToObj = JSON.parse(studentToString)
+
+// shows stubent is a string
+console.log(typeof studentToString);
+
+/*********** first entry **************/
+console.log(JSON.parse(studentToObj.name[0]));
+console.log(JSON.parse(studentToObj.height[0]));
+console.log(JSON.parse(studentToObj.age[0]));
+console.log(JSON.parse(studentToObj.school[0]));
+console.log(JSON.parse(studentToObj.major[0]));
+
+/*********** second entry **************/
+
+// console.log(JSON.parse(studentToObj.name[1]));
+// console.log(JSON.parse(studentToObj.height[1]));
+// console.log(JSON.parse(studentToObj.age[1]));
+// console.log(JSON.parse(studentToObj.school[1]));
+// console.log(JSON.parse(studentToObj.major[1]));
+
+// shows student is an object
+console.log(typeof studentToObj);
